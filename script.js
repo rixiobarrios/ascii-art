2c3985406e9c8942998b26b28176cbf1cde3d80c
aspittel <ali07cat07@gmail.com>
2018-03-17 13:42:03 -0400

    Update script.js

--- script.js
+++ script.js
@@ -21,3 +21,33 @@ for (let index = 0, spacer = Math.ceil(MAX / 2); index <= MAX; index += 2, space
   console.log(breads.padStart(padding + spacer))
   breads += '🍞🍞'
 }
+
+// IV. Calendar
+const MONTHS = [
+  'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+  'Jul', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'
+]
+
+const DAYS_OF_THE_WEEK = [
+  '  Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'
+]
+
+const NUM_WEEKS = 31 / 7
+
+for (let month in MONTHS) {
+  console.log('--------------------------------------------')
+  console.log(MONTHS[month].padStart(22))
+  console.log('--------------------------------------------')
+  console.log(DAYS_OF_THE_WEEK.join(' '))
+
+  for (let week = 0; week < NUM_WEEKS; week++) {
+    let weekStr = ' '
+    for (let day = 1; day <= 7; day++) {
+      let monthDay = day + (week * 7)
+      // exit out of the loop if the day of the month is greater than 31
+      if (monthDay > 31) break
+      weekStr += monthDay.toString().padStart(4)
+    }
+    console.log(weekStr)
+  }
+}
